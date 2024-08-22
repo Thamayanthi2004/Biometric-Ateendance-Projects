242382c961a5ee5433a5710829206f7b39df86cc
Thamayanthi2004 <146456968+Thamayanthi2004@users.noreply.github.com>
2024-08-22 20:36:36 +0530

    Update and rename README.md to App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginPage from './LoginPage';
+import Header from "../src/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
